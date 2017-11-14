13053f8a9c8ed5aadfda2cde176b731091cbaf55
jiikko <n905i.1214@gmail.com>
2017-11-14 21:51:11 +0900

    Impliment render png

--- app.rb
+++ app.rb
@@ -4,21 +4,18 @@ require 'open-uri'
 require 'open3'
 
 get '/' do
-  body = <<~EOH
-digraph g{
-login_3[
-  style = "filled";
-]
-graph[
-  layout = dot;
-]
-"login_2" -> "login_1";
-"login_3" -> "login_1";
-"login_4" -> "login_2";
-}
-  EOH
   cmd = ['dot', '-Tpng']
-  out, err, status =  Open3.capture3(*cmd, binmode: true, stdin_data: body)
-  content_type "image/png"
-  return out
+  out, err, status =  Open3.capture3(*cmd, binmode: true, stdin_data: params[:body])
+  case status.to_i
+  when 0
+    content_type "image/png"
+    out
+  else
+    content_type "text/plain"
+    <<~EOH
+      エラーになりました
+      -----------------
+      #{err}
+    EOH
+  end
 end
