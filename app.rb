edcf03c0aca7a585c4ba4130bba479e074badf2d
jiikko <n905i.1214@gmail.com>
2017-11-14 21:44:14 +0900

    Impliment render png

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
 require 'sinatra'
 require 'uri'
 require 'open-uri'
+require 'open3'
 
 get '/' do
-  return 'hello'
+  body = <<~EOH
+digraph g{
+login_3[
+  style = "filled";
+]
+graph[
+  layout = dot;
+]
+"login_2" -> "login_1";
+"login_3" -> "login_1";
+"login_4" -> "login_2";
+}
+  EOH
+  cmd = ['dot', '-Tpng']
+  out, err, status =  Open3.capture3(*cmd, binmode: true, stdin_data: body)
+  content_type "image/png"
+  return out
 end
